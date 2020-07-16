d50c392ff123dfa43adf7d78f5fddf7f03b06b64
Andrea Carola <andrea.carola@itstalentfactory.onmicrosoft.com>
2020-07-16 09:37:39 +0200

    StringCalculator: first commit

---
+++ StringCalculator/StringCalculator/StringCalculatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public class StringCalculatorService
+    {
+        public static void Add(string strInput)
+        {
+            
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorUnitTest/StringCalculatorTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using StringCalculator;
+
+namespace StringCalculatorUnitTest
+{
+    public class StringCalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void InputString_ShouldReturn_TheEquivalentNumber()
+        {
+            Assert.Pass();
+        }
+    }
+}
