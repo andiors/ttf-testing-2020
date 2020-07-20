0c6dc920d0ae98f6007e6a7ca1f057a2a2f50137
Andrea Carola <andrea.carola@itstalentfactory.onmicrosoft.com>
2020-07-20 12:05:40 +0200

    NotesClassic: implement datetime wrapper

---
+++ NotesClassic/Notes/Clock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes
+{
+    public class Clock : IClock
+    {
+        public DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+}
